fd2b7e320371a9b81d758e5c9b15b58212e64f71
Riyad Hosen <riyadhosen@Riyads-MacBook-Air.local>
2024-03-26 15:32:34 -0700

    setup tailwind and daisyui with react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className=' text-red-500 text-5xl font-bold'>This is Charlie</h1>
+     
+    </>
+  )
+}
+
+export default App
